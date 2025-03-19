9f9e1874b9f8b3e875f5964f93a76de52c77bf13
William Siafalis-Tsiatsios <161271230+DarkBolt101@users.noreply.github.com>
2025-03-19 14:45:09 +1100

    Update enemy2.gd

--- Assets/Scripts/Enemy/enemy2.gd
+++ Assets/Scripts/Enemy/enemy2.gd
@@ -7,6 +7,7 @@ var OrbitDirection
 var BulletSpeed = 600
 var Bullet = preload("res://Scenes/Misc/bullet.tscn")
 var xp_scene = preload("res://Scenes/Misc/xp.tscn")
+var Target = "Player"
 
 func _ready():
 	add_to_group("Enemy")
@@ -25,7 +26,17 @@ func orbit_direction_change():
 	print("ORBIT DIRECTION CHANGE")
 	
 func _physics_process(_delta):
-	var Player = get_parent().get_node("Player")
+	var Player = get_parent().get_node(Target)
+	if is_in_group("Enemy"):
+		Player = get_parent().get_node(Target)
+	elif is_in_group("Minion") and get_tree().get_nodes_in_group("Enemy").size() > 0 and is_instance_valid(Target):
+		Player = get_parent().get_node(Target)
+	elif is_in_group("Minion") and get_tree().get_nodes_in_group("Enemy").size() > 0 and not is_instance_valid(Target):
+		if get_tree().get_nodes_in_group("Enemy").size() > 0:
+			Target = get_tree().get_nodes_in_group("Enemy")[0].get_path()
+			Player = get_parent().get_node(Target)
+	else:
+		Player = get_parent().get_node(self.get_path())
 	
 	var Direction = (Player.position - position).normalized()
 	
@@ -54,12 +65,25 @@ func start_firing_timer():
 	timer.start()
 
 func fire():
-	var Player = get_parent().get_node("Player")
+	var Player = get_parent().get_node(Target)
+	if is_in_group("Enemy"):
+		Player = get_parent().get_node(Target)
+	elif is_in_group("Minion") and get_tree().get_nodes_in_group("Enemy").size() > 0 and is_instance_valid(Target):
+		Player = get_parent().get_node(Target)
+	elif is_in_group("Minion") and get_tree().get_nodes_in_group("Enemy").size() > 0 and not is_instance_valid(Target):
+		if get_tree().get_nodes_in_group("Enemy").size() > 0:
+			Target = get_tree().get_nodes_in_group("Enemy")[0].get_path()
+			Player = get_parent().get_node(Target)
+	else:
+		Player = get_parent().get_node(self.get_path())
 	
 	var BulletInstance = Bullet.instantiate()
 	BulletInstance.name = "Laser_" + str(randi())  # Assigns a unique named
 	BulletInstance.get_node("Sprite2D").modulate = Color(1, 0, 0)  # Red color
-	BulletInstance.add_to_group("Laser")
+	if is_in_group("Enemy"):
+		BulletInstance.add_to_group("Laser")
+	elif is_in_group("Minion"):
+		BulletInstance.add_to_group("Bullet")
 	var Direction = (Player.position - position).normalized()
 	var OffsetDistance = 30
 	BulletInstance.position = position + (Direction * OffsetDistance)
@@ -75,8 +99,22 @@ func drop_xp():
 	get_parent().add_child(xp)
 
 func _on_area_2d_body_entered(body: Node2D):
-	if body.is_in_group("Bullet") or body.is_in_group("Minion"): # Fixed
-		for i in range(3):
+	if is_in_group("Enemy") and (body.is_in_group("Bullet") or body.is_in_group("Minion")):
+		for i in range(1):
 			drop_xp()
 		body.queue_free()
 		queue_free()
+	elif body.is_in_group("Spell"):
+		remove_from_group("Enemy")
+		add_to_group("Minion")
+		var sprite = get_node("Sprite2D")
+		sprite.modulate = Color(0.8, 0.9, 1)
+		if get_tree().get_nodes_in_group("Enemy").size() > 0:
+			Target = get_tree().get_nodes_in_group("Enemy")[0].get_path()
+			print(Target)
+	elif is_in_group("Minion") and body.is_in_group("Enemy"):
+		await get_tree().process_frame
+		if not is_instance_valid(body) or not body.get_parent():
+			call_deferred("queue_free")
+
+# 0.8, 0.9, 1
