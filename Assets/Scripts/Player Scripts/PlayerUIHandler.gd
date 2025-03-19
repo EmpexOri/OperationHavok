417a79d4edc04a5f5344f8b8dcebd3a50f54766a
EmpexOri <Oliver.rayward@icloud.com>
2025-03-19 15:07:54 +1100

    Global Controls Probably Working

--- Assets/Scripts/Player Scripts/PlayerUIHandler.gd
+++ Assets/Scripts/Player Scripts/PlayerUIHandler.gd
@@ -2,16 +2,21 @@ extends Node
 
 @onready var HealthBar: TextureProgressBar = $UIContainer/HealthBar
 @onready var XPBar: TextureProgressBar = $UIContainer/XPBar
+@onready var LevelLabel: Label = $UIContainer/LevelLabel
+@onready var ClassLabel: Label = $UIContainer/ClassLabel
 
 func _ready():
 	UpdateHealthBar()
 	UpdateXPBar()
+	UpdateClassInfo()
 
 func _process(_delta):
 	UpdateHealthBar()
 	UpdateXPBar()
+	UpdateClassInfo()
 
 func UpdateHealthBar():
+	print("HP:", Global.PlayerHP, " / ", Global.PlayerHPMax)
 	HealthBar.max_value = Global.PlayerHPMax
 	HealthBar.value = Global.PlayerHP
 
@@ -21,5 +26,13 @@ func UpdateXPBar():
 	var XP = Global.ClassData[CurrentClass]["XP"]
 	var XPNeeded = Global.XPRequiredForLevel(Level)
 
+	print("XP:", XP, " / ", XPNeeded)
 	XPBar.max_value = XPNeeded
 	XPBar.value = XP
+
+func UpdateClassInfo():
+	var CurrentClass = Global.CurrentClass
+	var Level = Global.ClassData[CurrentClass]["Level"]
+
+	LevelLabel.text = "Level: " + str(Level)
+	ClassLabel.text = "Class: " + CurrentClass
