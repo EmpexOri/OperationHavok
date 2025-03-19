1669118b34df7384441b7dc2d3a326ec9a11a280
William Siafalis-Tsiatsios <161271230+DarkBolt101@users.noreply.github.com>
2025-03-19 15:57:47 +1100

    constant damage when still being hit (a bit scuffed)

--- Assets/Scripts/Player Scripts/player.gd
+++ Assets/Scripts/Player Scripts/player.gd
@@ -6,18 +6,27 @@ var Bullet = preload("res://Scenes/Misc/bullet.tscn")
 var Class = preload("res://Assets/Scripts/Player Scripts/Technomancer.gd").new()
 var Mode = "fire"
 var Trigger_Timer = Timer.new()
+var Damage_Timer = Timer.new()
 
 func _ready():
 	add_to_group("player")
 	trigger()
+	damage_timer()
 	
 func trigger():
 	Trigger_Timer = Timer.new()
 	Trigger_Timer.wait_time = 0.25
-	Trigger_Timer.one_shot = false  # Timer only goes once
+	Trigger_Timer.one_shot = false
 	Trigger_Timer.connect("timeout", Callable(self, Mode)) # Executes the spawn function once timer has ended
 	add_child(Trigger_Timer)
 	Trigger_Timer.start()
+	
+func damage_timer():
+	Damage_Timer = Timer.new()
+	Damage_Timer.wait_time = 1
+	Damage_Timer.one_shot = false
+	Damage_Timer.connect("timeout", Callable(self, "deal_damage"))
+	add_child(Damage_Timer)
 	
 func _physics_process(_delta):
 	var Motion = Vector2()
@@ -66,6 +75,12 @@ func fire():
 	BulletInstance.linear_velocity = Direction * BulletSpeed
 	get_tree().get_root().call_deferred("add_child", BulletInstance)
 	
+func deal_damage():
+	Global.PlayerHP -= 10
+	if Global.PlayerHP <= 0:
+		Global.PlayerHP = Global.PlayerHPMax
+		kill()
+
 func kill():
 	get_tree().reload_current_scene()
 
@@ -73,6 +88,7 @@ func kill():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy") or body.is_in_group("Laser"):
 		Global.PlayerHP -= 10
-	if Global.PlayerHP <= 0:
-		Global.PlayerHP = Global.PlayerHPMax
-		kill()
+		Damage_Timer.start()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	Damage_Timer.stop()
