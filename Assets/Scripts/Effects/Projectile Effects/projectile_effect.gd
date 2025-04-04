cf2ad77d47e801887c08bcecc911b4e410b8714d
Greg Williams <gwilliams8923@gmail.com>
2025-04-04 11:13:50 +1100

    Update projectile_effect.gd

--- Assets/Scripts/Effects/Projectile Effects/projectile_effect.gd
+++ Assets/Scripts/Effects/Projectile Effects/projectile_effect.gd
@@ -3,11 +3,14 @@ class_name ProjectileEffect
 
 @export var effect_name: String
 
+# Call when initialising an effect
+func setup(projectile: Projectile):
+	pass
+
+# Called every frame
 func process_effect(projectile: Projectile, delta: float):
 	pass
-	
+
+# Called when projectile has a collision
 func on_hit(projectile: Projectile, body: Node2D):
-	pass
-	
-func setup(projectile: Projectile):
 	pass
