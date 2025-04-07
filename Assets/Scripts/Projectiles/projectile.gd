4ff5bf6203952afa6c5a322cb20d2a2b6aa55285
Greg Williams <gwilliams8923@gmail.com>
2025-04-07 10:16:38 +1000

    Fix enemy specific logic in projectile, fix lifetime

--- Assets/Scripts/Projectiles/projectile.gd
+++ Assets/Scripts/Projectiles/projectile.gd
@@ -11,7 +11,7 @@ class_name Projectile
 # Current values for projectile stats - these will be modified by effects
 var speed: float
 var damage: float
-var lifetime: float = 1
+var lifetime: float
 
 # Projectile velocity
 var velocity: Vector2 = Vector2.ZERO
@@ -27,6 +27,11 @@ var current_effects: Array[ProjectileEffect] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Initialise our stats from base stats
+	speed = base_speed
+	damage = base_damage
+	lifetime = base_lifetime
+	
 	# Connect signals
 	lifetime_timer.wait_time = lifetime
 	lifetime_timer.timeout.connect(queue_free) # Destroy when lifetime expires
@@ -65,12 +70,6 @@ func start(start_position: Vector2, direction: Vector2, entity_owner: String, p_
 	if entity_owner == "Enemy" or entity_owner == "Enemy2" or entity_owner == "Enemy4":
 		collision_layer = 4  # Enemy projectile layer
 		collision_mask = 1  # Only collides with players
-		
-		if entity_owner == "Enemy2":
-			sprite_2d.modulate = Color(0.3, 0.5, 1)
-		if entity_owner == "Enemy4":
-			sprite_2d.modulate = Color(0.9, 0.9, 0)
-		
 	elif entity_owner == "Player":
 		collision_layer = 3  # Player projectile layer
 		collision_mask = 2  # Only collides with enemies
