62b781112601968241c5f1db8d9a144c627d0090
William Siafalis-Tsiatsios <161271230+DarkBolt101@users.noreply.github.com>
2025-04-12 17:42:11 +1000

    they are now spawning in (can't be hit by bullets tho)

--- Assets/Scripts/Misc/prototype_world.gd
+++ Assets/Scripts/Misc/prototype_world.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-var Enemy = preload("res://Scenes/Misc/enemy.tscn")
-var Enemy2 = preload("res://Scenes/Misc/enemy_2.tscn")
-var Enemy3 = preload("res://Scenes/Misc/enemy_3.tscn")
-var Enemy4 = preload("res://Scenes/Misc/enemy_4.tscn")
+var Enemy = preload("res://Scenes/Enemy/Hordling.tscn")
+var Enemy2 = preload("res://Scenes/Enemy/Spewling.tscn")
+#var Enemy3 = preload("res://Scenes/Misc/enemy_3.tscn")
+var Enemy4 = preload("res://Scenes/Enemy/Needling.tscn")
 
 @onready var PausedLabel: Label = $PausedLayer/Title
 @onready var ResumeButton: Button = $PausedLayer/ResumeButton
@@ -13,7 +13,7 @@ var Enemy4 = preload("res://Scenes/Misc/enemy_4.tscn")
 func _ready():
 	start_spawn_timer1()
 	start_spawn_timer2()
-	start_spawn_timer3()
+	#start_spawn_timer3()
 	start_spawn_timer4()
 	
 	# Pause screen assets
@@ -86,24 +86,24 @@ func spawn_enemy2():
 
 	###############################################################################################################
 	
-func start_spawn_timer3():
-	var timer = Timer.new()
-	timer.wait_time = randf_range(6, 9) # nice
-	timer.one_shot = true  # Timer only goes once
-	timer.connect("timeout", Callable(self, "spawn_enemy3")) # Executes the spawn function once timer has ended
-	add_child(timer)
-	timer.start()
-
-func spawn_enemy3():
-	var EnemyInstance = Enemy3.instantiate()
-	EnemyInstance.name = "Enemy_" + str(randi()) # Assigns a unique named
-	var viewport = get_viewport_rect().size
-	var random_x = randf_range(0, viewport.x)  # Random X position
-	var random_y = randf_range(0, viewport.y)  # Random Y position
-	EnemyInstance.position = Vector2(randf_range(100, random_x), randf_range(100, random_y))
-	add_child(EnemyInstance)
-	
-	start_spawn_timer3()
+#func start_spawn_timer3():
+	#var timer = Timer.new()
+	#timer.wait_time = randf_range(6, 9) # nice
+	#timer.one_shot = true  # Timer only goes once
+	#timer.connect("timeout", Callable(self, "spawn_enemy3")) # Executes the spawn function once timer has ended
+	#add_child(timer)
+	#timer.start()
+#
+#func spawn_enemy3():
+	#var EnemyInstance = Enemy3.instantiate()
+	#EnemyInstance.name = "Enemy_" + str(randi()) # Assigns a unique named
+	#var viewport = get_viewport_rect().size
+	#var random_x = randf_range(0, viewport.x)  # Random X position
+	#var random_y = randf_range(0, viewport.y)  # Random Y position
+	#EnemyInstance.position = Vector2(randf_range(100, random_x), randf_range(100, random_y))
+	#add_child(EnemyInstance)
+	#
+	#start_spawn_timer3()
 	
 	###############################################################################################################
 	
