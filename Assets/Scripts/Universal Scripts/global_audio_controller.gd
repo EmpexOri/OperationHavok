c7f7adeee1b7d66f7f19bb55384e9d921161b474
William Siafalis-Tsiatsios <161271230+DarkBolt101@users.noreply.github.com>
2025-04-12 17:20:18 +1000

    Enemies Starting

--- Assets/Scripts/Universal Scripts/global_audio_controller.gd
+++ Assets/Scripts/Universal Scripts/global_audio_controller.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 # Set up functions to play each track when called
 func LevelOneMusic():
+	$Music/Level1Soundtrack.volume_db = -10
 	$Music/Level1Soundtrack.play()
 
 func PauseMenuMusic():
+	$Music/PauseMenuSoundtrack.volume_db = -10
 	$Music/PauseMenuSoundtrack.play()
 	
 func DeathSound():
