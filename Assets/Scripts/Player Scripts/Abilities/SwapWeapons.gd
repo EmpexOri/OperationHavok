81f66f6af5a981b6d26343212fbd0e27d7f06546
EmpexOri <Oliver.rayward@icloud.com>
2025-04-14 19:05:44 +1000

    SwapWeapons ability now functional

--- Assets/Scripts/Player Scripts/Abilities/SwapWeapons.gd
+++ Assets/Scripts/Player Scripts/Abilities/SwapWeapons.gd
@@ -1,5 +1,41 @@
-extends Node
+extends Node2D
+class_name WeaponSwap
 
-func activate(player):
-	print("Switched Weapons Activated!")
-	# Create a grenade entity and throw it
+signal perk_finished(index: int)
+
+@export var cooldown_time: float = 1.0
+
+var smg_scene := preload("res://Scenes/Weapons/smg.tscn")
+var shotgun_scene := preload("res://Scenes/Weapons/shotgun.tscn")
+
+var perk_index: int
+
+func activate(player, index = -1):
+	perk_index = index
+
+	if not player or not player.CurrentWeapon:
+		queue_free()
+		return
+
+	var current_weapon_scene = player.CurrentWeapon.scene_file_path
+
+	# Swap logic
+	if current_weapon_scene == smg_scene.resource_path:
+		player.equip_weapon(shotgun_scene)
+		print("Swapped to Shotgun!")
+	else:
+		player.equip_weapon(smg_scene)
+		print("Swapped to SMG!")
+
+	# Start cooldown timer
+	var cooldown_timer := Timer.new()
+	cooldown_timer.one_shot = true
+	cooldown_timer.wait_time = cooldown_time
+	cooldown_timer.timeout.connect(_cooldown_complete)
+	add_child(cooldown_timer)
+	cooldown_timer.start()
+
+func _cooldown_complete():
+	print("Weapon swap cooldown complete.")
+	emit_signal("perk_finished", perk_index)
+	queue_free()
