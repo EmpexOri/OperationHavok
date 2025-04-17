522c06fdc6663721444a0eb1bf0d094e0d5e62ee
EmpexOri <Oliver.rayward@icloud.com>
2025-04-17 12:53:50 +1000

    Imported Tileset

---
+++ Assets/Tilesets/Carpark_Area_Tilemap/Carpark_Tileset_Autotile.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Spawn_Area_Tileset_Autotile" tilewidth="16" tileheight="16" tilecount="24" columns="6">
+ <image source="Carpark_Tileset_Autotile.png" width="96" height="64"/>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
