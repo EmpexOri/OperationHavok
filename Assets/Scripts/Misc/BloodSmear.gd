48eb1ed6c5649783b8f6ad27d5b6d53deb397488
EmpexOri <Oliver.rayward@icloud.com>
2025-04-28 00:28:44 +1000

    BloodSmear Optimisation Code (Speleld Worng)

---
+++ Assets/Scripts/Misc/BloodSmear.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+@export var lifetime: float = 30.0  # Time before it starts fading
+@export var fade_time: float = 5.0   # Time it takes to fade out
+
+var timer: Timer
+var fading := false
+
+func _ready():
+	timer = $Timer
+	timer.wait_time = lifetime
+	timer.timeout.connect(start_fading)
+	timer.start()
+
+func start_fading():
+	fading = true
+	timer.wait_time = fade_time
+	timer.timeout.connect(queue_free)
+	timer.start()
+
+func _process(delta):
+	if fading:
+		var alpha_decrease = delta / fade_time
+		modulate.a = max(0.0, modulate.a - alpha_decrease)
