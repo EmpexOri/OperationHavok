9922c734cd60c1c8a1f355668f1ec38f45ce4b17
GoogleyEyes <froginapond10@gmail.com>
2025-05-12 21:45:36 +1000

    Skill tree fix - Part 1

--- Assets/Scripts/Menu Scripts/commando_skill_tree.gd
+++ Assets/Scripts/Menu Scripts/commando_skill_tree.gd
@@ -6,3 +6,8 @@ func _ready():
 func update_perk_points_label():
 	var perk_points = GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"]
 	$PerkPointsLabel.text = "Perk points: %d" % perk_points
+
+
+func _on_back_button_pressed() -> void:
+	GlobalAudioController.ClickSound()
+	get_tree().change_scene_to_file("res://Scenes/MenuScene.tscn")
