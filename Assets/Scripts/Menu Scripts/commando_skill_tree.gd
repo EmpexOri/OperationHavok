ef897fafdcf5edcb4d05c5764495f8ba00418c27
GoogleyEyes <froginapond10@gmail.com>
2025-05-12 17:59:41 +1000

    Points counter in skill tree linked to buttons

--- Assets/Scripts/Menu Scripts/commando_skill_tree.gd
+++ Assets/Scripts/Menu Scripts/commando_skill_tree.gd
@@ -2,7 +2,13 @@ extends CanvasLayer
 
 func _ready():
 	update_perk_points_label()
+	connect_skill_buttons(self)
 
 func update_perk_points_label():
-	var perk_points = GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"]
-	$PerkPointsLabel.text = "Perk points: %d" % perk_points
+	$PerkPointsLabel.text = "Perk points: %d" % GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"]
+
+func connect_skill_buttons(node):
+	for child in node.get_children():
+		if child is SkillButton:
+			child.connect("perk_point_used", Callable(self, "update_perk_points_label"))
+		connect_skill_buttons(child)  # Recursively connect nested children
--- Assets/Scripts/Menu Scripts/skill_button.gd
+++ Assets/Scripts/Menu Scripts/skill_button.gd
@@ -1,19 +1,22 @@
 extends TextureButton
+class_name SkillButton
+
+signal perk_point_used
 
 @onready var panel = $Panel
 @onready var label = $MarginContainer/Label
-
-var Points = GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"]
 
 var lockedCondition = "Locked":
 	set(value):
 		lockedCondition = value
 		label.text = str(lockedCondition)
 
+func _ready():
+	label.text = str(lockedCondition)
 
 func _on_pressed() -> void:
-	if Points > 0:
-		Points -= 1
-		print(Points)
+	if GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"] > 0 and lockedCondition == "Locked":
+		GlobalPlayer.ClassData[GlobalPlayer.CurrentClass]["PerkPoints"] -= 1
 		panel.show_behind_parent = true
 		lockedCondition = ""
+		emit_signal("perk_point_used")
