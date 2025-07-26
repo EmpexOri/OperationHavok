ab886b465f36319e4e02729c10f92a1ac6c5183a
GoogleyEyes <froginapond10@gmail.com>
2025-07-26 14:54:29 +1000

    All weapons in dictionary

--- Assets/Scripts/Universal Scripts/WeaponData.gd
+++ Assets/Scripts/Universal Scripts/WeaponData.gd
@@ -8,5 +8,7 @@ const weapon_scenes = {
 	"Shotgun": preload("res://Prefabs/CodePrefabs/Weapons/Shotgun.tscn"),
 	"DragonShotgun": preload("res://Prefabs/CodePrefabs/Weapons/DragonShotgun.tscn"),
 	"Sniper": preload("res://Prefabs/CodePrefabs/Weapons/Sniper.tscn"),
-	
+	"LightningLauncher": preload("res://Prefabs/CodePrefabs/Weapons/lightning_launcher.tscn"),
+	"RocketLauncher": preload("res://Prefabs/CodePrefabs/Weapons/rocket_launcher.tscn"),
+	"RocketMinigun": preload("res://Prefabs/CodePrefabs/Weapons/RocketMinigun.tscn")
 }
