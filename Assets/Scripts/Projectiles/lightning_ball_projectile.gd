a72d20ab31d70ecf09c3f103e02b69c2f2830b33
Greg Williams <gwilliams8923@gmail.com>
2025-08-14 11:44:41 +1000

    Add explosion to typhoon cannon

--- Assets/Scripts/Projectiles/lightning_ball_projectile.gd
+++ Assets/Scripts/Projectiles/lightning_ball_projectile.gd
@@ -2,7 +2,24 @@ extends Projectile
 class_name LightningBallProjectile
 
 @onready var ball_anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var explosion_ref: Grenade = $Explosion
+
+var exploded: bool = false
 
 func _ready() -> void:
 	super._ready()
 	ball_anim.play()
+	explosion_ref.start(global_position, Vector2.ZERO)
+	explosion_ref.explosion_delay = base_lifetime - 1
+	
+func _physics_process(delta: float) -> void:
+	if explosion_ref:
+		explosion_ref.global_position = global_position
+		
+func _handle_movement(delta: float):
+	if not exploded:
+		position += velocity * delta
+
+func _on_explosion_timer_timeout() -> void:
+	ball_anim.visible = false
+	exploded = true
