7a4ebb9cdc80a7f8c2240f434a788e33bde219fa
EmpexOri <Oliver.rayward@icloud.com>
2025-08-20 21:51:46 +1000

    Connected Properly to Skill Tree

--- Assets/Scripts/Universal Scripts/GlobalPlayer.gd
+++ Assets/Scripts/Universal Scripts/GlobalPlayer.gd
@@ -16,7 +16,7 @@ var ClassData = {
 	},
 	"Commando": {
 		"Level": 3, "XP": 0, "PerkPoints": 5, "PerPointsSpent": 0, "MoveSpeed": 150,
-		"Abilities": ["SwapWeapons", "GrenadeThrow", "TyphoonCannon"], "UnlockedAbilities": []
+		"Abilities": ["SwapWeapons", "GrenadeThrow", "RocketLauncher"], "UnlockedAbilities": []
 	},
 	"Fleshthing": {
 		"Level": 1, "XP": 0, "PerkPoints": 0, "PerPointsSpent": 0, "MoveSpeed": 150, 
