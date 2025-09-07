5f50e0b6e60171720ac36a4bf4dc031bf9dddc6e
Greg Williams <gwilliams8923@gmail.com>
2025-09-07 15:09:33 +1000

    Add initial assets and prefabs for home run

---
+++ Assets/Scripts/Player Scripts/Weapons/Misc/HomeRunExplosion.gd
@@ -0,0 +1,25 @@
+extends SuperGrenade
+class_name HomeRunExplosion
+
+# Assign audio streams in inspector or preload them here
+@export var explosion_sound: AudioStream = preload("res://Assets/Sound/SFX/WeaponSFX/Grenade/TyphoonBoom.wav")
+
+var has_landed: bool = false
+var land_sound_played: bool = false
+
+func _ready():
+	# Call parent _ready so all explosion logic still works
+	super._ready()
+
+func _explode():
+	if exploding:
+		return
+	exploding = true
+	velocity = Vector2.ZERO
+
+	# Explosion visuals and damage
+	_do_explosion_effects()
+
+	# Play unique explosion sound
+	if explosion_sound:
+		GlobalAudioController.PlayFromPlayerSFX(explosion_sound)
