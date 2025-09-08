459338279037c9b334727ad74d8f7165bf7b8d19
Greg Williams <gwilliams8923@gmail.com>
2025-09-08 15:05:21 +1000

    Fixes for home runs effects

--- Assets/Scripts/Effects/Projectile Effects/phys_damage_up_bounce.gd
+++ Assets/Scripts/Effects/Projectile Effects/phys_damage_up_bounce.gd
@@ -8,18 +8,19 @@ func _init():
 
 func on_hit(projectile, body: Node2D, collision: KinematicCollision2D = null):
 	if collision == null:
-		return false # Ignore non phys collision
+		return true # Ignore non phys collision
 	
 	var bounce_effect = _find_bounce_effect(projectile.current_effects) # Get the bounce effect
 	
 	# Get the current multiplier for the projectile instance and increase by damage_multiplier_per_bounce
 	if is_instance_valid(bounce_effect) and bounce_effect.current_bounces < bounce_effect.max_bounces:
+		print(bounce_effect.current_bounces)
 		var current_multi: float = projectile.get_damage_multiplier()
 		var new_multi: float = current_multi + damage_multiplier_per_bounce
 		projectile.set_damage_multiplier(new_multi)
-		return false # Not responsible for destruction
+		return true # Not responsible for destruction
 	
-	return false # Not responsible for destruction
+	return true # Not responsible for destruction
 
 # Helper function to find the bounce effect in the projectile's effect list
 func _find_bounce_effect(effects: Array[ProjectileEffect]) -> PhysProjectileBounces:
