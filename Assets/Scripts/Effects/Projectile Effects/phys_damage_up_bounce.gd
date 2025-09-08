a94ff970f30de1a23c1e8cfe46133f4a747225f0
Greg Williams <gwilliams8923@gmail.com>
2025-09-08 10:39:13 +1000

    Add effect for increasing damage on bounce

---
+++ Assets/Scripts/Effects/Projectile Effects/phys_damage_up_bounce.gd
@@ -0,0 +1,29 @@
+extends ProjectileEffect
+class_name DamageUpOnBounceEffect
+
+@export var damage_multiplier_per_bounce: float = 0.5
+
+func _init():
+	effect_name = "Damage Up On Bounce"
+
+func on_hit(projectile, body: Node2D, collision: KinematicCollision2D = null):
+	if collision == null:
+		return false # Ignore non phys collision
+	
+	var bounce_effect = _find_bounce_effect(projectile.current_effects) # Get the bounce effect
+	
+	# Get the current multiplier for the projectile instance and increase by damage_multiplier_per_bounce
+	if is_instance_valid(bounce_effect) and bounce_effect.current_bounces < bounce_effect.max_bounces:
+		var current_multi: float = projectile.get_damage_multiplier()
+		var new_multi: float = current_multi + damage_multiplier_per_bounce
+		projectile.set_damage_multiplier(new_multi)
+		return false # Not responsible for destruction
+	
+	return false # Not responsible for destruction
+
+# Helper function to find the bounce effect in the projectile's effect list
+func _find_bounce_effect(effects: Array[ProjectileEffect]) -> PhysProjectileBounces:
+	for effect in effects:
+		if effect is PhysProjectileBounces:
+			return effect
+	return null
