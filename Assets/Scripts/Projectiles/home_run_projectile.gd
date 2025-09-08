d15955e331ace37d1943300c23527deb68af69e3
Greg Williams <gwilliams8923@gmail.com>
2025-09-08 10:56:10 +1000

    Add derived script for home run projectile

---
+++ Assets/Scripts/Projectiles/home_run_projectile.gd
@@ -0,0 +1,11 @@
+extends PhysicsProjectile
+class_name HomeRunProjectile
+
+@onready var projectile_sprite: Sprite2D = $Sprite2D
+@onready var explosion_ref: HomeRunExplosion = $Explosion
+
+func explode() -> void:
+	if is_instance_valid(explosion_ref) and explosion_ref.has_method("_explode"):
+		self.velocity = Vector2.ZERO
+		projectile_sprite.visible = false
+		explosion_ref._explode()
