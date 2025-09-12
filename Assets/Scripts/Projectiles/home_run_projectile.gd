9cbe9e6aba416a0e033c061e2ce4d0d5bca288df
Greg Williams <gwilliams8923@gmail.com>
2025-09-13 08:53:48 +1000

    Fix explosion timing on home run grenade

--- Assets/Scripts/Projectiles/home_run_projectile.gd
+++ Assets/Scripts/Projectiles/home_run_projectile.gd
@@ -4,6 +4,11 @@ class_name HomeRunProjectile
 @onready var projectile_sprite: Sprite2D = $Sprite2D
 @onready var explosion_ref: HomeRunExplosion = $Explosion
 
+func _ready() -> void:
+	super._ready()
+	explosion_ref.start(global_position, Vector2.ZERO)
+	explosion_ref.explosion_delay = base_lifetime - 1
+
 func explode() -> void:
 	if is_instance_valid(explosion_ref) and explosion_ref.has_method("_explode"):
 		self.velocity = Vector2.ZERO
