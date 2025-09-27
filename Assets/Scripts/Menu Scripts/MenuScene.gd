956d48e96a93fb9ce49ccc89495e191b13de8b6a
GoogleyEyes <froginapond10@gmail.com>
2025-09-27 12:09:17 +1000

    Attract mode vid (Bad quality test)

--- Assets/Scripts/Menu Scripts/MenuScene.gd
+++ Assets/Scripts/Menu Scripts/MenuScene.gd
@@ -5,18 +5,10 @@ extends Node2D
 
 var SkipLoading = false
 
-func _ready():
-	$PlayButton.grab_focus()
-	
+func _ready():	
 	if not GlobalAudioController.is_main_menu_music_playing():
 		GlobalAudioController.StopAllMusic()
 		GlobalAudioController.PlayMainMenuMusic()
-
-# Causes problems, will sort out after demo
-#func _input(Event):
-#	if Input.is_action_just_pressed("SkipLoad"):
-#		LoadingSprite.queue_free()
-#		LoadingFinished.queue_free()
 
 func _on_play_button_pressed() -> void:
 	GlobalAudioController.ClickSound()
@@ -41,13 +33,3 @@ func _on_quit_button_pressed() -> void:
 	var quitTimer = 0.15
 	await get_tree().create_timer(quitTimer).timeout
 	get_tree().quit()
-
-
-func _on_skill_tree_button_pressed() -> void:
-	GlobalAudioController.ClickSound()
-	get_tree().change_scene_to_file("res://Scenes/Options/CommandoSkillTree.tscn")
-
-
-func _on_options_button_pressed() -> void:
-	GlobalAudioController.ClickSound()
-	get_tree().change_scene_to_file("res:///Scenes/Options/OptionsScene.tscn")
