06fd5d29c6d1f34a05471c63883aeb07fa422dd0
EmpexOri <Oliver.rayward@icloud.com>
2025-10-07 20:17:41 +1100

    Difficulty

--- Assets/Scripts/Universal Scripts/GlobalPlayer.gd
+++ Assets/Scripts/Universal Scripts/GlobalPlayer.gd
@@ -76,7 +76,7 @@ func LevelUp():
 
 func AddHp(Amount: int):
 	var Level = ClassData[CurrentClass]["Level"]
-	var HpGainMultiplier = 1 + ((Level) * 1.15)
+	var HpGainMultiplier = 1
 	var AdjustedAmount = int(Amount * HpGainMultiplier)
 	PlayerHP = min(PlayerHP + AdjustedAmount, PlayerHPMax)
 	UpdateHealthBar()
